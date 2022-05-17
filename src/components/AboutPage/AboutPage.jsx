6b9b0773da3df1e83c240543040cb2db88bedbdc
Clay Webb <90583942+claywebb5@users.noreply.github.com>
2022-05-16 23:14:32 -0500

    Initialize feature-last-minute

---
+++ src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+// ---------< COMPONENT VIEW IMPORTS >----------------
+import About from './About';
+import Packages from './Packages';
+// ---------< MUI IMPORTS >----------------
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
+import TabPanel from '@material-ui/lab/TabPanel';
+import Container from '@mui/material/Container';
+
+
+
+function AboutPage() {
+  // ========< TABS LOGIC >===================
+  const [value, setValue] = useState('1');
+
+  // ------< TABS CLICK HANDLER >-------------
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  // sx={{ bgcolor: "#41414c", marginBottom: 1 }}
+  return (
+    <>
+      <Container>
+        <Box sx={{ bgcolor: "#fff", width: '100%', typography: 'body1' }}>
+          <TabContext value={value}>
+            {/* TABS TITLE  */}
+            <Box sx={{ bgcolor: "#6d6e71", borderBottom: 1, borderColor: 'divider' }}>
+              {/* <TabList onChange={handleChange} centered indicatorColor='#ace23a'> */}
+              <TabList onChange={handleChange} centered indicatorColor='secondary'>
+                <Tab label="About" value="1" />
+                <Tab label="Classes & Packages" value="2" />
+              </TabList>
+            </Box>
+            {/* TABS CONTENT (ABOUT) */}
+            <TabPanel value="1">
+              <About />
+            </TabPanel>
+            {/* TABS CONTENT (CLASSES & PACKAGES) */}
+            <TabPanel value="2">
+              <Packages />
+            </TabPanel>
+          </TabContext>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ src/redux/sagas/trainer.saga.js
@@ -0,0 +1,49 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+// =============***< (GET) ALL ATTENDEES FOR A CLASS >***==============================
+function* fetchAttendance(action){
+    // TRAINER/ADMIN ONLY
+    // Will send a request to the trainer router to get all users signed up for a class
+    
+    try {
+        const attendance = yield axios.get(`/api/trainer/attendance/${action.payload}`) // * Goes to SERVER "trainer.router"
+        yield put({ type: 'SET_ATTENDEES', payload: attendance.data }); // * Goes to REDUCERS "attendees.reducer"
+    } catch {
+        console.log('Get all attendees error');
+    }
+}
+
+// =============***< (PUT) UPDATE CLASS DETAILS >***===================================
+function* updateDetails (action){
+    // TRAINER/ADMIN ONLY
+    // Will send a request to the trainer router to update the details of a specific class
+    try{
+        yield axios.put(`/api/trainer/edit-class/${action.payload.id}`, action.payload); // * Goes to SERVER "trainer.router"
+        yield put({type: 'FETCH_CLASS_DETAILS', payload: action.payload.id}) // * Goes to REDUCERS "classDetails.reducer"
+    } catch (error){
+        console.log('Error editing class details', error);
+    }
+}
+
+// =============***< (PUT) UPDATE CLASS ATTENDANCE >***=======================================
+function* updateAttendance(action){
+    // TRAINER/ADMIN ONLY
+    // Will send a request to the trainer router to update attendance with those who arrived for the class
+    console.log(action.payload);
+    
+    try{
+        yield axios.put(`/api/trainer/check-in/${action.payload.id}`, action.payload.attendees); // * Goes to SERVER "trainer.router"
+        yield put({type: 'FETCH_ATTENDANCE', payload: action.payload.id }) // * Goes to THIS SAGA "trainer.saga" (fetchAttendance)
+    } catch (error){
+        console.log('Error updating checked in status', error);
+    }
+}
+
+function* trainerSaga(){
+    yield takeLatest('FETCH_ATTENDANCE', fetchAttendance);
+    yield takeLatest('UPDATE_CLASS_DETAILS', updateDetails);
+    yield takeLatest('UPDATE_ATTENDANCE', updateAttendance);
+}
+
+export default trainerSaga;
