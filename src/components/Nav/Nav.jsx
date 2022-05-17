c0cae24886e913e47e30da3951a73e32613ab6a8
Clay Webb <90583942+claywebb5@users.noreply.github.com>
2022-05-16 23:14:13 -0500

    Initialize feature-class-saga

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import './Nav.css';
+import { useSelector } from 'react-redux';
+
+// ------------------ *** NEED TO SPECIFY BETWEEN AUTH LEVEL *** ----------------------------
+
+function Nav() {
+  const user = useSelector((store) => store.user);
+
+  return (
+    <div className="nav">
+      <Link to="/home">
+        <h2 className="nav-title">FitTruk (Nav Bar)</h2>
+      </Link>
+      <div>
+        {/* If no user is logged in, show these links */}
+        {!user.id && (
+          // If there's no user, show login/registration links
+          <Link className="navLink" to="/login">
+            <button>Login / Register</button>
+          </Link>
+        )}
+
+        {/* If a user is logged in, show these links */}
+        {user.id && (
+          <>
+          <Link className="navLink" to="/all-classes">
+              <button>**LOGO**</button>
+            </Link>
+
+            <Link className="navLink" to="/all-classes">
+              <button>All Classes</button>
+            </Link>
+
+            <Link className="navLink" to="/my-classes">
+              <button>My Classes</button>
+            </Link>
+            
+            <Link className="navLink" to="/class-details">
+              <button>Class Details</button>
+            </Link>
+
+            <Link className="navLink" to="/attendees">
+              <button>Class Attendees</button>
+            </Link> 
+            <Link className="navLink" to="/create-class">
+              <button>Create Class</button>
+            </Link>
+            <Link className="navLink" to="/personal-info">
+              <button>Personal Info</button>
+            </Link>
+            <Link className="navLink" to="/info">
+              <button>Info Page</button>
+            </Link>
+            <Link className="navLink" to="/personal-info">
+              <button>**PROFILE PIC**</button>
+            </Link>
+          </>
+        )}
+
+        <Link className="navLink" to="/about">
+          <button>About</button>
+        </Link>
+
+
+        {/*------ TEMPORARY DIV, DELETE WHEN NECESSARY --------*/}
+        <div // This is DIV is temporary, just to help differentiate where the nav bar ends and the page begins. Delete when needed ----------------------------
+        style={{marginBottom: 20, backgroundColor: '#8bc34a'}}>
+        ---- -----
+        </div>
+        {/*------ TEMPORARY DIV, DELETE WHEN NECESSARY ---------*/}
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/redux/sagas/customer.saga.js
@@ -0,0 +1,81 @@
+import {put, takeLatest, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+// ** This saga file will listen for the following actions:
+    // *[x] Getting all the customers personal info (Personal Info/Profile)
+    // *[x] Update customers personal info (Personal Info/Profile)
+    // *[x] Getting all the customers registered classes (My Classes)
+    // *[x] Adding a new class to their reservations (My/All Classes/Class Details)
+    // *[x] Removing a class from their reservations (My Classes/Class Details)
+
+
+
+// =============***< (GET ALL) CUSTOMER INFO >***=======================================
+function* fetchCustomerInfo(){
+    try {
+        console.log('In fetchCustomerInfo, about to axios.get all the customer personal info');
+        const customerInfoResponse = yield axios.get('/api/user');  
+        console.log('Getting all customer info:', customerInfoResponse.data);
+        yield put({ type: 'SET_USER_INFO', payload: customerInfoResponse.data });
+    } catch {
+        console.log('Error trying to fetchCustomerInfo in sagas!');
+    }
+}
+
+// =============***< (PUT) UPDATE CUSTOMER INFO >***=======================================
+function* updateCustomerInfo(action){
+    try {
+        console.log('In updateCustomerInfo the action.payload is:', action.payload);
+        yield axios.put(`/api/customer/pronouns/${action.payload.id}`, action.payload);
+        yield put({type: 'SET_USER_INFO'});
+    } catch (error){
+        console.log('Error updating customer info:', error)
+    }
+
+}
+
+// =============***< (GET ALL) REGISTERED CLASSES >***=======================================
+function* fetchCustomerClass() {
+    try {
+        console.log('In fetchCustomerClass, about to axios.get all the customers registered classes');
+        const customerClassResponse = yield axios.get('/api/customer');
+        console.log('Getting all registered classes:', customerClassResponse.data);
+        yield put({ type: 'SET_MY_CLASSES', payload: customerClassResponse.data });
+    } catch {
+        console.log('Error trying to fetchCustomerClass in sagas!');
+    }
+}
+
+// =============***< (POST) ADD CUSTOMER CLASS RESERVATION >***=======================================
+function* addReservation(action){
+    try {
+        console.log('The action.payload for adding a new class reservation is:', action.payload);
+        yield axios.post('/api/customer', action.payload);
+        yield put({ type: 'FETCH_CUSTOMER_CLASS' });
+    } catch (error) {
+        console.log('Error adding a new class reservation', error);
+    }    
+}
+
+// =============***< (DELETE) REMOVE CUSTOMER CLASS RESERVATION >***=======================================
+function* removeReservation(action){
+    try {
+        console.log('The action.payload in removing a class reservation is:', action.payload)
+        yield axios.delete(`/api/customer/delete/${action.payload.id}`, action.payload);
+        yield put({ type: 'FETCH_CUSTOMER_CLASS' });
+    } catch (error) {
+        console.log('Error removing a class reservation', error);
+    }   
+}
+
+
+// =============*< WATCHER SAGA >*=======================================
+function* customerSaga() {
+  yield takeLatest('FETCH_CUSTOMER_CLASS', fetchCustomerClass);
+  yield takeLatest('FETCH_CUSTOMER_INFO', fetchCustomerInfo);
+  yield takeLatest('ADD_RESERVATION', addReservation);
+  yield takeLatest('REMOVE_RESERVATION', removeReservation);
+  yield takeLatest('UPDATE_CUSTOMER_INFO', updateCustomerInfo);
+}
+
+export default customerSaga;
