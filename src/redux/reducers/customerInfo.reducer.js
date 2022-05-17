e2aba4e7371b0601a4bcb2b4d785acb87f288396
Clay Webb <90583942+claywebb5@users.noreply.github.com>
2022-05-16 23:14:13 -0500

    Initialize file

---
+++ src/redux/reducers/customerInfo.reducer.js
@@ -0,0 +1,12 @@
+const customerInfo = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_USER_INFO':
+        return action.payload;
+        
+    
+      default:
+        return state;
+    }
+  };
+
+  export default customerInfo;
