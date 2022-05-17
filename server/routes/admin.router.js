c5a782ff1d3d30a673504f88afa6d34ff4a538f7
Clay Webb <90583942+claywebb5@users.noreply.github.com>
2022-05-16 23:14:22 -0500

    Initialize profile-pic

---
+++ server/routes/admin.router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+// -------------------------- Admin creates new class (POST)
+
+router.post('/new-class', (req, res) => {
+  // if (req.isAuthenticated()) {
+    const queryText = `INSERT INTO "classes" ("classname", "description", "trainer_user_id", "date", "start_time", "end_time", "street", "city", "state", "zip", "class_size")
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`;
+    pool.query(queryText, [req.body.classname, req.body.description, req.body.trainer_user_id, req.body.date, req.body.start_time, req.body.end_time, req.body.street, req.body.city, req.body.state, req.body.zip, req.body.class_size])
+
+      .then((result) => {
+        res.send(result.rows)
+        console.log('Created a new class!', result.rows);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      })
+  // } else {
+  //   res.sendStatus(403);
+  // }
+});
+
+// -------------------------- Admin GET trainer list (trainers have access level of 2)
+router.get('/', (req, res) => {
+
+  if (req.isAuthenticated()) {
+    let queryText = `select "name", "id", "profile_image"
+      from "user"
+      where "access_level" = 2;`;
+    pool.query(queryText).then((result) => {
+      res.send(result.rows)
+    }).catch((error) => {
+      console.log(error)
+      res.sendStatus(500)
+    })
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+
+
+
+module.exports = router;
---
+++ src/components/UserPage/UserPage.jsx
@@ -0,0 +1,179 @@
+// ==================================< PERSONAL INFO PAGE >====================================================
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { deepOrange, deepPurple } from '@mui/material/colors';
+
+function UserPage() {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // Default user info from original repo
+  const user = useSelector((store) => store.user);
+
+
+  //  ============<>=============
+  let userObj = {
+    id: user.id,
+    username: user.username,
+    password: user.password,
+    name: user.name,
+    email: user.email,
+    street: user.street,
+    city: user.city,
+    state: user.state,
+    zip: user.zip,
+    dob: user.dob,
+    phone_number: user.phone_number,
+    pronouns: user.pronouns,
+    emergency_name: user.emergency_name,
+    emergency_number: user.emergency_number,
+    access_level: user.access_level
+  }
+
+  const [editUser, setEditUser] = useState(userObj);
+  const name = user.name.toUpperCase()
+  const words = name.split(' ');
+  const initials = [];
+
+  for (const i of words) {
+    initials.push(i[0])
+  }
+
+
+  const handleTest = () => {
+    console.log('The user is:', user);
+    console.log('The userObj is:', userObj);
+    console.log('The state of editUser is:', editUser);
+
+  }
+
+  //  ============< Pronoun Change >=============
+  const handlePronounChange = (event) => {
+    console.log('New Pronoun:', event.target.value);
+
+    setEditUser({ ...editUser, pronouns: event.target.value });
+    console.log('In handlePronounChange');
+
+  };
+
+  //  ============< Address Change >=============
+  const handleChangeStreet = (event) => {
+    setEditUser({ ...editUser, street: event.target.value });
+  };
+
+  const handleChangeCity = (event) => {
+    setEditUser({ ...editUser, city: event.target.value });
+  };
+
+  const handleChangeState = (event) => {
+    setEditUser({ ...editUser, state: event.target.value });
+  };
+
+  const handleChangeZip = (event) => {
+    setEditUser({ ...editUser, zip: event.target.value });
+  };
+
+  //  ============< Submit >==============================================
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let updatedUser = editUser;
+    updatedUser = { ...updatedUser };
+    console.log('Clicked Submit');
+    console.log('Updated user info is:', updatedUser);
+    dispatch({
+      type: 'UPDATE_CUSTOMER_INFO',
+      payload: updatedUser
+    });
+  }
+
+  const handleReturnClick = () => {
+    history.goBack();
+    console.log('Clicked Cancel');
+  }
+
+
+  return (
+    <>
+      <h1><u>Personal Info Page</u></h1>
+
+      {/* <button onClick={handleTest}>Test userObj</button> */}
+
+
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+
+          <div> {/* CAN EDIT  */}
+            {/* <h2>*Profile Image Here*</h2> */}
+
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>{initials.join('')}</Avatar>
+
+          </div>
+
+          <h2>Welcome, {user.name}!</h2>
+          <div> {/* CAN EDIT  */}
+            <p><b>Pronouns:</b> {user.pronouns}</p>
+            <select onChange={handlePronounChange} value={editUser.pronouns}>
+              <option value="He/Him"> He/Him</option>
+              <option value="She/Her"> She/Her</option>
+              <option value="They/Them"> They/Them</option>
+            </select>
+          </div>
+          <p><b>Email:</b> {user.email}</p>
+          <p><b>Number:</b> {user.phone_number}</p>
+
+          <div> {/* CAN EDIT  */}
+            <p><b>Street:</b> {user.street}</p>
+            <input
+              type="text"
+              placeholder={user.street}
+              value={editUser.street}
+              onChange={handleChangeStreet}
+            />
+          </div>
+          <div>
+            <p><b>City:</b> {user.city}</p>
+            <input
+              type="text"
+              placeholder={user.city}
+              value={editUser.city}
+              onChange={handleChangeCity}
+            />
+          </div>
+          <div>
+            <p><b>State:</b> {user.state}</p>
+            <input
+              type="text"
+              placeholder={user.state}
+              value={editUser.state}
+              onChange={handleChangeState}
+            />
+          </div>
+          <div>
+            <p><b>Zip:</b> {user.zip}</p>
+            <input
+              type="text"
+              placeholder={user.zip}
+              value={editUser.zip}
+              onChange={handleChangeZip}
+            />
+          </div>
+
+          <p>------- In case of emergencies ---------</p>
+          <p><b>Emergency Contact:</b> {user.emergency_name}</p>
+          <p><b>Number:</b> {user.emergency_number}</p>
+          <button onClick={handleReturnClick}>Cancel</button>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+
+    </>
+  );
+}
+
+
+export default UserPage;
