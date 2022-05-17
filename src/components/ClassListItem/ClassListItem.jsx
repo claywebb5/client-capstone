c49f6d738c7614e2e4c06249e2c56da1c0812551
Clay Webb <90583942+claywebb5@users.noreply.github.com>
2022-05-16 23:14:11 -0500

    Initialize feature-search

---
+++ src/components/ClassListItem/ClassListItem.jsx
@@ -0,0 +1,22 @@
+// import { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function ClassListItem({event, i}) {
+  const history = useHistory();
+
+  const handleEventClick = () => {
+    history.push('/class-details')
+  }
+
+  return (
+    <>
+      <li key={i} onClick={handleEventClick}>
+        {event.date}
+        {event.start_time}-{event.end_time}
+        {event.classname} with {event.trainer_user_id}
+      </li>
+    </>
+  )
+}
+export default ClassListItem;
---
+++ src/components/UserPage/UserPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+function UserPage() {
+  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const user = useSelector((store) => store.user);
+  return (
+    <div className="container">
+      <h2>Welcome, {user.username}!</h2>
+      <p>Your ID is: {user.id}</p>
+    </div>
+  );
+}
+
+// this allows us to use <App /> in index.js
+export default UserPage;
